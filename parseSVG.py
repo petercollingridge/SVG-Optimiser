1cd55ba8faba6c100f7ad2754397c778e36b609c
Peter Collingridge <peter.collingridge@gmail.com>
2012-01-26 21:29:39 +0000

    Create Element tree object to overwrite the _write() method.

--- parseSVG.py
+++ parseSVG.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from xml.etree.ElementTree import ElementTree
+import xml.etree.ElementTree as ET
 import re
 
 # Regex
@@ -11,12 +11,78 @@ position_attributes = {"rect":    (["x", "y"]),
                        "ellipse": (["cx", "cy"]),
                        "line":    (["x1", "y1", "x2", "y2"])}
 
+def fixtag(tag, namespaces):
+    # given a decorated tag (of the form {uri}tag), return prefixed
+    # tag and namespace declaration, if any
+    if isinstance(tag, ET.QName):
+        tag = tag.text
+    namespace_uri, tag = tag[1:].split("}", 1)
+    prefix = namespaces.get(namespace_uri)
+    if prefix is None:
+        prefix = ET._namespace_map.get(namespace_uri)
+        if prefix is None:
+            prefix = "ns%d" % len(namespaces)
+        namespaces[namespace_uri] = prefix
+        
+        if prefix == "xml":
+            xmlns = None
+        else:
+            xmlns = ("xmlns", namespace_uri)
+    else:
+        xmlns = None
+        
+    return tag, xmlns
+
 def printNode(node):
-    print node.tag
+    print ">", node.tag.split('}')[1]
+
+class SVGTree(ET.ElementTree):
+
+    def _write(self, file, node, encoding, namespaces):
+        """ Overwrite the normal method to avoid writing namespaces everywhere. """
+    
+        # write XML to file
+        tag = node.tag
+        if tag is ET.Comment:
+            file.write("<!-- %s -->" % _escape_cdata(node.text, encoding))
+        elif tag is ET.ProcessingInstruction:
+            file.write("<?%s?>" % _escape_cdata(node.text, encoding))
+        else:
+            items = node.items()
+            xmlns_items = [] # new namespaces in this scope
+            try:
+                if isinstance(tag, ET.QName) or tag[:1] == "{":
+                    tag, xmlns = fixtag(tag, namespaces)
+                    if xmlns: xmlns_items.append(xmlns)
+            except TypeError:
+                _raise_serialization_error(tag)
+            file.write("<" + ET._encode(tag, encoding))
+            
+            if items or xmlns_items:
+                items.sort() # lexical order
+                for k, v in items:
+                    file.write(" %s=\"%s\"" % (ET._encode(k, encoding), ET._escape_attrib(v, encoding)))
+                for k, v in xmlns_items:
+                    file.write(" %s=\"%s\"" % (ET._encode(k, encoding), ET._escape_attrib(v, encoding)))
+                    
+            if node.text or len(node):
+                file.write(">")
+                if node.text:
+                    file.write(ET._escape_cdata(node.text, encoding))
+                for n in node:
+                    self._write(file, n, encoding, namespaces)
+                file.write("</" + ET._encode(tag, encoding) + ">")
+                
+            else:
+                file.write(" />")
+            for k, v in xmlns_items:
+                del namespaces[v]
+        if node.tail:
+            file.write(ET._escape_cdata(node.tail, encoding))
 
 class CleanSVG:
     def __init__(self, svgfile=None):
-        self.tree = ElementTree()
+        self.tree = SVGTree()
         self.root = None
         
         self.num_format = "%s"
@@ -68,6 +134,7 @@ class CleanSVG:
 def main():
     import os
     filename = os.path.join('examples', 'translations.svg')
+    
     s = CleanSVG(filename)
     s.findTransforms()
     s.write('%s_test.svg' % filename[:-4])
